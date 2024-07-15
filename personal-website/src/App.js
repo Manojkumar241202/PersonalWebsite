468d831f8248c9fb6362fa240d93a9404c41929a
Manojkumar241202 <126048786+Manojkumar241202@users.noreply.github.com>
2024-07-16 00:15:59 +0530

    feature/menu

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,10 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-
-function App() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faInfoCircle, faBlog, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import Home from './Home';
+import About from './About';
+import Blog from './Blog';
+import Contact from './Contact';
+import IconComponent from './menu/IconComponent';
+const App = () => {
   return (
-    <div className="App">
+    <div className="icon-container">
+      <IconComponent iconClass="fas fa-home" label="HOME" >
+      </IconComponent>
+      <IconComponent iconClass="fas fa-user" label="ABOUT" >
+      </IconComponent>
+      <IconComponent iconClass="fas fa-briefcase" label="PORTFOLIO"></IconComponent>
+      <IconComponent iconClass="fas fa-envelope" label="Contact" >
 
-    </div>
+      </IconComponent>
+      <IconComponent iconClass="fas fa-pencil" label="BLOG" >
+      </IconComponent>
+      </div>
   );
 }
 

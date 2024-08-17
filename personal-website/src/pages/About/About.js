73cce3a6693893855954e6dde35dc0427559390a
Manojkumar241202 <126048786+Manojkumar241202@users.noreply.github.com>
2024-08-17 20:41:03 +0530

    testing

--- personal-website/src/pages/About/About.js
+++ personal-website/src/pages/About/About.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import SectionTitle from '../../utils/TitleSection';
 import "./About.css";
 import SwipeButton from "../../utils/SwipeButton"
 import { handleDownload } from '../../utils/handlers/HandleDownload';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const About = () => {
+  const experience_grid= useRef(null);
+  const [experience_grid_columns, set_experience_grid_columns] = useState(0);
+
+  useEffect(() => {
+    if (experience_grid.current) {
+        // Count the number of children elements
+        const count = experience_grid.current.children.length;;
+
+        // Set the number of columns based on the count
+        if (count < 4) {
+          set_experience_grid_columns(1);
+        } else {
+          set_experience_grid_columns(2);
+        }
+    }
+  }, []);
+
+  const experience_grid_dynamic_columns_style={
+    display: 'grid',
+    gridTemplateColumns: `repeat(${experience_grid_columns}, 1fr)`,
+  };
   return (
     <div className="about-page">
       <SectionTitle background_text="RESUME" grey_text="ABOUT " yellow_text="ME " />
@@ -50,7 +72,25 @@ const About = () => {
           </ul>
         </div>
       </div>
-      
+      <div className="experience" >
+        <h2>EXPERIENCE</h2>
+        <ul className="experience-list" ref={experience_grid} style={experience_grid_dynamic_columns_style}>
+            <li className="experience-item">
+                <div className="icon">
+                    <i className="fa fa-briefcase"></i>
+                </div>
+                <div className="experience-info">
+                    <div className="time">
+                        JUN 2024 - PRESENT
+                    </div>
+                    <h5 className="role">
+                        SOFTWARE ENGINEER
+                        <span> - NFERENCE</span>
+                    </h5>
+                </div>
+            </li>
+        </ul>
+    </div>
     </div>
   );
 }

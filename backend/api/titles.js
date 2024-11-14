e8f4789fad2564f5e08f4e685c0690aad202225b
Manojkumar241202 <126048786+Manojkumar241202@users.noreply.github.com>
2024-11-14 10:03:19 +0530

    added database url

--- backend/api/titles.js
+++ backend/api/titles.js
@@ -6,8 +6,8 @@ if (!admin.apps.length) {
           projectId: process.env.FIREBASE_PROJECT_ID,
           privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          databaseURL: process.env.FIREBASE_DATABASE_URL
         }),
-        databaseURL: process.env.FIREBASE_DATABASE_UR
       });
       
   }
--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ admin.initializeApp({
     projectId: process.env.FIREBASE_PROJECT_ID,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    databaseURL: process.env.FIREBASE_DATABASE_URL
   }),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 const app = express();
